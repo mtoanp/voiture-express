b1e9e1c546bf85812bccff53728e84abb6232eaa
mtoanp <mtoanp@outlook.com>
2025-05-17 22:28:17 +0200

    refactor: structure app

---
+++ src/shared/components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import "./navbar.scss";
+
+// import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  // const { isAuthenticated, logout } = useAuth();
+  // const navigate = useNavigate();
+
+  return (
+    <nav className="navbar w-full bg-gradient-to-r from-purple-500 to-blue-500 shadow-md px-6 py-1 text-white flex justify-between items-center">
+      navbar
+    </nav>
+  );
+};
+
+export default Navbar;
