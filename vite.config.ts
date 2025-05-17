1cb1ec62b09f53bb5758615e86a023f77b178554
mtoanp <mtoanp@outlook.com>
2025-05-17 23:40:34 +0200

    chore(app): add __APP_VERSION__

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,5 +8,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5000,
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version), // ← gets injected globally
   },
 });
