f3fec7ede3f95d9bd4f7a9785c40ea9250572890
mtoanp <mtoanp@outlook.com>
2025-05-19 11:11:08 +0200

    refactor: re organize structure

--- src/core/contexts/auth.context.tsx
+++ src/core/contexts/auth.context.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import type { AuthUser } from "./auth-user";
+import type { AuthUser } from "../../features/auth/auth-user";
 
 export interface AuthContextType {
   currentUser: AuthUser | null; // ✅ now accepts null
